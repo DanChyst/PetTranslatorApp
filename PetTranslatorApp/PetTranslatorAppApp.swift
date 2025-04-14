69871602884b3b013747e0eca39d40a140ee138b
Dan Chystov <dan.chystov@gmail.com>
2025-04-13 22:41:56 -0400

    Initial Commit

---
+++ PetTranslatorApp/PetTranslatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTranslatorAppApp.swift
+//  PetTranslatorApp
+//
+//  Created by Dan on 2025-04-13.
+//
+
+import SwiftUI
+
+@main
+struct PetTranslatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
